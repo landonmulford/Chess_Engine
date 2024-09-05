54b5bf252be1943f6350d45f1b039251ebeb73dc
landonmulford <165755614+landonmulford@users.noreply.github.com>
2024-09-05 13:31:56 -0700

    Update Piece.hpp

--- Piece.hpp
+++ Piece.hpp
@@ -9,8 +9,6 @@
 #define Piece_hpp
 using namespace std;
 #include <vector>
-
-//#include <iostream>
 #include <stdio.h>
 
 class Piece{
@@ -21,18 +19,11 @@ public:
         posN=pos11;
         posL=pos21;
     };
-    //int temp[2];
     virtual int getValue();
     bool color;
     int posL;
     int posN;
-    //void displayPiece();
-    
-    //bool makeMove(int pos_N, int pos_L, Piece[8][8]);
     virtual Piece* makeCopy();
-    void addMoves(int pN, int pL);
-    void editMoves(Piece[8][8]);
-    Piece* moveToBoard(int posN, int posL);
     virtual char getType();
     virtual vector<vector<int>> generateMoves(Piece* board[8][8]) const;
     
