cced698c837614f831a770bbff2fd1a865616021
landonmulford <165755614+landonmulford@users.noreply.github.com>
2024-09-05 13:30:27 -0700

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -7,30 +7,17 @@
 #include <iostream>
 #include "Piece.hpp"
 
-void Piece::addMoves(int pN, int pL){
-   //temp[0]=pN;
-    //temp[1]=pL;
-        //System.out.println("["+temp[0]+" "+temp[1]+"]");
-    //legalMoves.add(temp);
-      
-}
+//all virtual functions, should never run
 int Piece::getValue(){
-    /*if(type=="Pawn"){return 1;}
-    if(type=="Knight"||type=="Bishop"){return 3;}
-    if(type=="Rook"){return 5;}
-    if(type=="Queen"){return 9;}
-    if(type=="King"){return 15;}*/
-    return -1;
-      
+   
+    return -1;   
 }
  vector<vector<int>> Piece::generateMoves(Piece* board[8][8]) const{
-    
-    cout<<"This is running the Piece version of generateMoves";
-    vector<vector<int>> cheers;
-    return cheers;
+    vector<vector<int>> dummy;
+    return dummy;
 }
 char Piece::getType(){
-    return 'L';
+    return 'Z';
 }
 Piece* Piece::makeCopy(){
     return nullptr;
