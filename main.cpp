c7965d665e1ec2c2e2293b7e910f8aef4f8644d1
landonmulford <165755614+landonmulford@users.noreply.github.com>
2024-09-05 13:54:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@
 #include "Rook.hpp"
 #include "Queen.hpp"
 #include "King.hpp"
-//maybe do piece instead of Piece*, could improve big O
-//Potential issues: white to move black to move, loading and outputting board, giving good recs, hella things
 
 int main(int argc, const char * argv[]) {
     
@@ -27,43 +25,9 @@ int main(int argc, const char * argv[]) {
         getline(cin, fen);
         Position* game=new Position(fen);
         Position* after=get<1>(game->recommend());
-        //game->recommend();
-        //Position* current=after;
-        /*while(current){
-            current->display();
-            current=current->next;
-        }*/
         after->display();
     }
-    //cout<<game->blackKing[0]<<" "<<game->blackKing[1];
-    //cout<<game->isCheck()<<game->isCheckMate();
-   
-   
-    //game->display();
-    //newPos works?, could be doing it repetively or theres an issue in the pawn shit
-    //Position* newPos=game->newPosition(game->board[1][0], {3,0});
-   // newPos->display();
-    //game->board[1][1]->generateMoves(game->board, true);
-    //recommend is returning null positions
-    
-    //Position* after=get<1>(game->recommend());
-    
-    //after->display();
-    /*Position* current=game;
-    while(current){
-        current->display();
-        current=current->next;
-    }
-  //Position* after=get<1>(get<1>((get<1>(game->recommend()))->recommend())->recommend());
-   //after->display();
-   // for(auto a:game->sortedPositions){
-   //     a.second->display();
-  //  }
-   
-  //  if(afterMove==nullptr){
-  //      cout<<"Fuck";
- //  }
-    //copy constructor*/
+
     return 0;
 };
 
