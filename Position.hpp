c6adb82baafbc077dc2698e3128a469cf8aafc34
landonmulford <165755614+landonmulford@users.noreply.github.com>
2024-09-05 13:49:08 -0700

    Update Position.hpp

--- Position.hpp
+++ Position.hpp
@@ -30,25 +30,51 @@ class Position{
 public:
     
     const static int depth=6;
-    //int currentDepth;
     int blackKing[2], whiteKing[2];
     Piece* board[8][8];
     int material;
     bool castleRights[4];
     int ep[2]={-1, -1};
 
-    
-    //Position* parent;
-    //Position* bestChild;
     unordered_set<Piece*> whitePieces;
     unordered_set<Piece*> blackPieces;
-    //unordered_map<Piece, unordered_map<int[2],Position*>> OrganizedMoves;
+
     map<double, Position*> sortedPositions;
     unordered_map<Piece*, vector<vector<int>>> unsortedPositions;
-   // int move[2];
-    
-    //int moveNum;
    bool whiteToMove;
+//default constructor
+     Position(){
+        whiteToMove=true;
+        board[0][0]=new Rook(true,0,0);
+        board[0][1]=new Knight(true,0,1);
+        board[0][2]=new Bishop(true,0,2);
+        board[0][3]=new Queen(true,0,3);
+        board[0][4]=new King(true,0,4);
+        board[0][5]=new Bishop(true,0,5);
+        board[0][6]=new Knight(true,0,6);
+        board[0][7]=new Rook(true,0,7);
+        board[7][0]=new Rook(false,7,0);
+        board[7][1]=new Knight(false,7,1);
+        board[7][2]=new Bishop(false,7,2);
+        board[7][3]=new Queen(false,7,3);
+        board[7][4]=new King(false,7,4);
+        board[7][5]=new Bishop(false,7,5);
+        board[7][6]=new Knight(false,7,6);
+        board[7][7]=new Rook(false,7,7);
+        for(int i=0; i<=7; i++){
+            board[1][i]= new Pawn(true,1,i);
+            board[6][i]= new Pawn(false,6,i);
+            }
+        for(int i=0; i<2; i++){
+            for(int j=0; j<8; j++){
+                whitePieces.insert(board[i][j]);
+                blackPieces.insert(board[7-i][j]);
+            }
+            
+            
+        }
+    };
+//constructor from input
     Position(const string& x){
         cout<<"string constructor called";
         int index=0;
@@ -87,7 +113,6 @@ public:
         if(x[index]=='w') whiteToMove=true;
         else whiteToMove=false;
         index++;
-        //insert castle stuff
         while(true){
             if(x[index]=='K') castleRights[0]=true;
             else if(x[index]=='Q') castleRights[1]=true;
@@ -105,42 +130,26 @@ public:
         
         
     }
+//copy constructor
     Position(const Position& old){
-        //cout<<"Copy constructor called";
         for (int x=0 ; x <8; x++){
           for (int y=0 ; y <8; y++){
             board[x][y] = old.board[x][y];
-              //THIS CALLS COPY ASSIGNMENT SO WILL NOT WORK
           }
         }
         for(int i=0; i<4; i++){
             castleRights[i]=old.castleRights[i];
         }
-        /*does this change things
-        color=*(new bool(old.color));
-        eval=*(new double(old.eval));
         
-        //this->blackKing=old->blackKing;
-       // this->whiteKing=old->whiteKing;
-        whitePieces=*(new unordered_set<Piece*>(old.whitePieces));
-        blackPieces=*(new unordered_set<Piece*>(old.blackPieces));
-        sortedPositions=*(new map<double, Position*>(old.sortedPositions));
-        currentDepth=old.currentDepth+1;
-        //this copies the sorted Positions, so maybe get rid of this or clear it at some point
-        //do I actually need to do all this for deep copy, or only for the stuff that is dynamic
-        */
         whiteToMove=old.whiteToMove;
-        //if(old.next==nullptr) next=nullptr;
-        //else next=new Position(*(old.next));
-        //currentDepth=old.currentDepth;
+      
        
         
         blackKing[0]=old.blackKing[0];
         blackKing[1]=old.blackKing[1];
         whiteKing[0]=old.whiteKing[0];
         whiteKing[1]=old.whiteKing[1];
-        //whitePieces=*(new unordered_set<Piece*>(old.whitePieces));
-        //blackPieces=*(new unordered_set<Piece*>(old.blackPieces));
+
         for(Piece* a:old.whitePieces){
             Piece* temp=a;
             whitePieces.insert(temp);
@@ -150,41 +159,24 @@ public:
             blackPieces.insert(temp);
         }
         
-        //NO COPY FOR SORteD POSItionS
-        /*for(auto a: sortedPositions){
-            double x=a.first;
-            Position* y=a.second;
-            sortedPositions.insert({x,y});
-        }*/
     };
     
    
-     
+     //destructor
     ~Position(){
         for(int i=0; i<8; i++){
             for(int j=0; j<8; j++){
                 delete board[i][j];
-                //interesting, when Position is a pointer no error, but this is a pointer regardless so i feel like this needs to be here, because they are reated with new, maybe theres no new when copy happens?
+                
             }
         }
-        //delete next;
-        //delete &color;
-        //delete &eval;
-        /*for(Piece* a: whitePieces){
-            delete a;
-        }
-        for(Piece* b: blackPieces){
-            delete b;
-        }*/
-        //delete &whitePieces;
-        //perhaps leakage inside white and black Pieces
-        //maybe I dont have to do this, or maybe I need to add this to constructor
-        //delete &blackPieces;
+       
         for(auto a:sortedPositions){
             delete a.second;
         }
         
     };
+//copy assignment operator
     Position& operator=(const Position& old)
     {
         for (int x=0 ; x <8; x++){
@@ -196,37 +188,13 @@ public:
         for(int i=0; i<4; i++){
             castleRights[i]=old.castleRights[i];
         }
-        //does this change things
-        /*color=*(new bool(old.color));
-        eval=*(new double(old.eval));
-        
-        //this->blackKing=old->blackKing;
-       // this->whiteKing=old->whiteKing;
-        whitePieces=*(new unordered_set<Piece*>(old.whitePieces));
-        blackPieces=*(new unordered_set<Piece*>(old.blackPieces));
-        sortedPositions=*(new map<double, Position*>(old.sortedPositions));
-        currentDepth=old.currentDepth+1;
-        //this copies the sorted Positions, so maybe get rid of this or clear it at some point
-        //do I actually need to do all this for deep copy, or only for the stuff that is dynamic
-        */
         whiteToMove=old.whiteToMove;
-        //currentDepth=old.currentDepth;
-        //next=new Position(*next);
         blackKing[0]=old.blackKing[0];
         blackKing[1]=old.blackKing[1];
         whiteKing[0]=old.whiteKing[0];
         whiteKing[1]=old.whiteKing[1];
         whitePieces=*(new unordered_set<Piece*>(old.whitePieces));
         blackPieces=*(new unordered_set<Piece*>(old.blackPieces));
-        /*
-        for(Piece* a:old.whitePieces){
-            Piece* temp=a;
-            whitePieces.insert(temp);
-        }
-        for(Piece* a:old.blackPieces){
-            Piece* temp=a;
-            blackPieces.insert(temp);
-        }*/
         for(auto a: sortedPositions){
             double x=a.first;
             Position* y=a.second;
@@ -235,83 +203,9 @@ public:
         
         return *this;
     };
-    /*Position(Position &&old)                                   // Move ctor
-    {
-        std::cout<<"Move constructor\n";
-        for (int x=0 ; x <8; x++){
-          for (int y=0 ; y <8; y++){
-            board[x][y] = old.board[x][y];
-              //THIS CALLS COPY ASSIGNMENT SO WILL NOT WORK
-          }
-        }
-        //does this change things
-        color=*(new bool(old.color));
-        eval=*(new double(old.eval));
-        
-        //this->blackKing=old->blackKing;
-       // this->whiteKing=old->whiteKing;
-        whitePieces=*(new unordered_set<Piece*>(old.whitePieces));
-        blackPieces=*(new unordered_set<Piece*>(old.blackPieces));
-        sortedPositions=*(new map<double, Position*>(old.sortedPositions));
-        currentDepth=old.currentDepth+1;
-        //this copies the sorted Positions, so maybe get rid of this or clear it at some point
-        //do I actually need to do all this for deep copy, or only for the stuff that is dynamic
-        
-        currentDepth=old.currentDepth+1;
-        eval=old.eval;
-        color=old.color;
-        blackKing[0]=old.blackKing[0];
-        blackKing[1]=old.blackKing[1];
-        whiteKing[0]=old.whiteKing[0];
-        whiteKing[1]=old.whiteKing[1];
-        for(Piece* a:old.whitePieces){
-            Piece* temp=a;
-            whitePieces.emplace(temp);
-        }
-        for(Piece* a:old.blackPieces){
-            Piece* temp=a;
-            blackPieces.emplace(temp);
-        }
-        
-        
-    };*/
     
     
-    Position(){
-        //currentDepth=0;
-        whiteToMove=true;
-        
-        board[0][0]=new Rook(true,0,0);
-        board[0][1]=new Knight(true,0,1);
-        board[0][2]=new Bishop(true,0,2);
-        board[0][3]=new Queen(true,0,3);
-        board[0][4]=new King(true,0,4);
-        board[0][5]=new Bishop(true,0,5);
-        board[0][6]=new Knight(true,0,6);
-        board[0][7]=new Rook(true,0,7);
-        board[7][0]=new Rook(false,7,0);
-        board[7][1]=new Knight(false,7,1);
-        board[7][2]=new Bishop(false,7,2);
-        board[7][3]=new Queen(false,7,3);
-        board[7][4]=new King(false,7,4);
-        board[7][5]=new Bishop(false,7,5);
-        board[7][6]=new Knight(false,7,6);
-        board[7][7]=new Rook(false,7,7);
-        for(int i=0; i<=7; i++){
-            board[1][i]= new Pawn(true,1,i);
-            board[6][i]= new Pawn(false,6,i);
-            //will it still be able to access the pawn generateMoves
-            }
-        for(int i=0; i<2; i++){
-            for(int j=0; j<8; j++){
-                whitePieces.insert(board[i][j]);
-                blackPieces.insert(board[7-i][j]);
-                //CHECK THIS MAKE SURE POINTER SHIT IS GOOD BUT IT SHOULD BE
-            }
-            
-            
-        }
-    };
+   
     
     
     bool isCheckMate();
@@ -322,7 +216,6 @@ public:
     double getEval();
     void generateSortedPositions();
     tuple<double, Position*> recommend(int currentDepth=0);
-    //double getAverageOfKidsEval();
     double getMaxEval();
     Position* newPosition(Piece* a,vector<int> x) const;
     bool rand(int rank, int total, bool white);
