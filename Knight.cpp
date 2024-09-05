79cb24636e75d8e64d76b38e2c581506de7aa3de
landonmulford <165755614+landonmulford@users.noreply.github.com>
2024-09-05 13:25:06 -0700

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -10,53 +10,44 @@ std::vector<vector<int>> Knight::generateMoves(Piece* board[8][8]) const{
     vector<vector<int>> result;
     if(posN+2<8&&posL+1<8){
           if(board[posN+2][posL+1]==nullptr||board[posN+2][posL+1]->color!=this->color){
-            //addMoves(posN+2,posL+1);
               result.push_back({posN+2, posL+1});
           }
         }
         if(posN+1<8&&posL+2<8){
           if(board[posN+1][posL+2]==nullptr||board[posN+1][posL+2]->color!=this->color){
-            //addMoves(posN+1,posL+2);
               result.push_back({posN+1, posL+2});
           }
         }
         if(posN-2>-1&&posL+1<8){
           if(board[posN-2][posL+1]==nullptr||board[posN-2][posL+1]->color!=this->color){
-            //addMoves(posN-2,posL+1);
             result.push_back({posN-2, posL+1});
           }
         }
         if(posN-1>-1&&posL+2<8){
           if(board[posN-1][posL+2]==nullptr||board[posN-1][posL+2]->color!=this->color){
-            //addMoves(posN-1,posL+2);
             result.push_back({posN-1, posL+2});
           }
         }
         if(posN+2<8&&posL-1>-1){
           if(board[posN+2][posL-1]==nullptr||board[posN+2][posL-1]->color!=this->color){
-            //addMoves(posN+2,posL-1);
               result.push_back({posN+2, posL-1});
           }
         }
         if(posN-1>-1&&posL+2<8){
           if(board[posN-1][posL+2]==nullptr||board[posN-1][posL+2]->color!=this->color){
-            //addMoves(posN-1,posL+2);
               result.push_back({posN-1, posL+2});
           }
         }
         if(posN-2>-1&&posL-1>-1){
           if(board[posN-2][posL-1]==nullptr||board[posN-2][posL-1]->color!=this->color){
-            //addMoves(posN-2,posL-1);
               result.push_back({posN-2, posL-1});
           }
         }
         if(posN-1>-1&&posL-2>-1){
           if(board[posN-1][posL-2]==nullptr||board[posN-1][posL-2]->color!=this->color){
-            //addMoves(posN-1,posL-2);
               result.push_back({posN-1, posL-2});
           }
         }
-        //if(a){editMoves(board);}
     return result;
       }
     
@@ -66,7 +57,6 @@ char Knight::getType(){
 }
 Piece* Knight::makeCopy(){
     Knight* x=new Knight(this->color, this->posN, this->posL);
-    
     return x;
 }
 int Knight::getValue() {return 3;}
